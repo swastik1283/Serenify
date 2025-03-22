80a8273ffe287abf2a8efe673f8d45ff1347480a
Samarora034 <sakshamarora034@gmail.com>
2025-03-23 00:32:46 +0530

    added survey page and survey questions in the json file

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Navbar from "./Navbar/Navbar";
 import Login from "./Login/page";
+import Survey from "./Survey/Survey";
 export default function Home() {
   return (
    
    <>
-   <Navbar/>
+   <Survey/>
    </>
   );
 }

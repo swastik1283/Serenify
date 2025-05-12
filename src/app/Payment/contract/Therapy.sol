4fd985eb92bac9b68c1a0121a1d92a349997821f
swastik1283 <swastik1283kot@gmail.com>
2025-05-12 22:59:40 +0530

    Contract

---
+++ src/app/Payment/contract/Therapy.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity^0.8.0;
+
+contract TherapyPay{
+    address public owner; 
+   mapping(address=>uint256)public payment;
+   mapping(address=>address)public doctorAddress;
+
+
+   event PaymentReceived(address indexed payer,address indexed doctors,uint256 amount);
+
+   constructor (){
+     owner=msg.sender;
+   }
+
+
+   function PayforTherapy(address doctors) external payable{
+     require(msg.value>0,"Payment must be greater than zero");
+     //marking doctor address for user
+     doctorAddress[msg.sender]=doctor;
+     //transfering payment
+     payable(doctor).transfer(msg.value);
+
+     emit PaymentReceived(msg.sender, doctors, msg.value);
+   }
+
+
+   function withdraw() external{
+    require (msg.sender==owner,"you are not the owner");
+    payable(owner).transfer(address(this).balance);
+   }
+}

c657b9ed64a382ae0b55affbe5ad144760f14412
swastik1283 <swastik1283kot@gmail.com>
2025-05-19 18:52:03 +0530

    Updated

--- src/app/PageLoader.js
+++ src/app/PageLoader.js
@@ -4,30 +4,29 @@ import React from 'react'
 import { useEffect,useState } from "react";
 import { usePathname } from "next/navigation";
 import {useDebounce} from  'use-debounce'
-import Image from 'next/image';
+import Image  from 'next/image';
 export default function PageLoader(){
     const pathname=usePathname();
     const[loading,setloading]=useState(false);
-    const [debouncedPathname]=useDebounce(pathname,200)
+    const [debouncedPathname]=useDebounce(pathname,500)
 
 
     useEffect(()=>{
         setloading(true);
         const timer=setTimeout(() => {
             setloading(false)
-        }, 600);
+        }, 3000);
         return ()=>clearTimeout(timer);
 
     },[debouncedPathname])
-    return loading?(
-        <div className="fixed inset-0 z-50  bg-white/80 flex items-center justify-center">
-            <Image
-                src="/assets/logoSerenify.png"
-                alt="loading"
-                width={96}
-                height={96}
-                className='w-24 h-24 object-contain'
-                priority
+    return loading ? (
+        <div className="fixed inset-0 z-50  bg-[#E4E6EA] flex items-center justify-center">
+            <video
+                src="/assets/Serenify.mp4"
+                           
+                className='w-full h-[90%] object-contain'
+                autoPlay muted loop
+
                 />
         </div>
     

59e22a0ad76e12273afe16b8d96f82fef2030b2b
swastik1283 <swastik1283kot@gmail.com>
2025-05-23 01:24:30 +0530

    fetches contract from blockchain

--- src/app/lib/web3/contract.js
+++ src/app/lib/web3/contract.js
@@ -1,4 +1,16 @@
+import React from "react";
 import { ethers } from "ethers";
 import abi from "./Therapyabi.js";
 
 const contractAddress="0x46F93E64A68f50A0b2Aa76e0A66b1078973cA313";
+
+
+export const getContract=()=>{
+   if(typeof window === "undefined") return null;
+   if(!window.ethereum) throw new Error("Metamask not installed")
+   
+    const provider= new ethers.providers.Web3Provider(window.ethereum);
+    const signer=provider.getSigner()
+    const contract =new ethers.Contract(contractAddress,abi,signer);
+    return contract;
+}

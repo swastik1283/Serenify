d0969ab25103320be8eb825fd0c420688b9d3f86
swastik1283 <swastik1283kot@gmail.com>
2025-05-24 12:33:16 +0530

    Contract.js

--- src/app/lib/web3/contract.js
+++ src/app/lib/web3/contract.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { ethers } from "ethers";
+import {BrowserProvider,Contract} from "ethers";
 import abi from "./Therapyabi.js";
 
 const contractAddress="0x46F93E64A68f50A0b2Aa76e0A66b1078973cA313";
 
 
-export const getContract=()=>{
+export const getContract= async()=>{
    if(typeof window === "undefined") return null;
    if(!window.ethereum) throw new Error("Metamask not installed")
    
-    const provider= new ethers.providers.Web3Provider(window.ethereum);
-    const signer=provider.getSigner()
-    const contract =new ethers.Contract(contractAddress,abi,signer);
+    const provider= new BrowserProvider(window.ethereum);
+    const signer= await provider.getSigner()
+    const contract =new Contract(contractAddress,abi,signer);
     return contract;
 }

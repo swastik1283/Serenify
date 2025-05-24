eff186b6b6fdbb3f9aa526d1fef7017fbe4ecb8e
swastik1283 <swastik1283kot@gmail.com>
2025-05-24 12:41:54 +0530

    Payment Page

--- src/app/Payment/page.js
+++ src/app/Payment/page.js
@@ -1,9 +1,13 @@
 "use client"
 import { useSearchParams } from "next/navigation";
 import { useEffect,useState } from "react"
-import {ethers} from "ethers";
+import handler from "../api/auth/Conversion/route";
+import { isAddress,parseEther } from "ethers";
 import { getContract } from "../lib/web3/contract";
+import { useRouter } from "next/navigation";
+
 export default function Payment() {
+    const router=useRouter()
     const searchParam=useSearchParams();
     const metaid=searchParam.get("metaid")
     const[doctors,setdoctors]=useState(null);
@@ -26,31 +30,63 @@ export default function Payment() {
               });
         }
 
-    },[metaid])
+    },[metaid]);
+
     const handlePayment=async()=>{
+     
           try{
             if((!doctors?.metaid) || (!doctors.fees)){
               return alert ("required metamask id or fees")
              
             }
-            if(!ethers.utils.isAddress(doctors.metaid)){
+            if(!isAddress(doctors.metaid)){
               return alert("invalid wallet address");
             }
-            const feeinEth=doctors.fees.toString();
+
+            const convres=await fetch(`/api/auth/Conversion?inrAmount=${doctors.fees}&symbol=eth`);
+            if (!convres.ok) {
+  throw new Error("Conversion API request failed.");
+}
+            const convdata= await convres.json();
+
+            const rate=parseFloat(convdata.amount)
+           
+               if(isNaN(rate)){
+                  return alert("failed to fetch ETH conversion rate")
+                  
+           }
+           const fee=(parseFloat(doctors.fees ))
+           if(isNaN(fee)){
+            return alert("doctor fees is not a valid number")
+           }
+            const feeinEth=(parseFloat(doctors.fees*rate).toFixed(8));
+        
+
+            console.log(`converter INR${doctors.fees} to ETH ${feeinEth}`)
+
+
             await window.ethereum.request({method:"eth_requestAccounts"})
-            const contract=getContract()
+
+            const contract= await getContract()
+
             if(!contract) return alert("Smart contract not found");
+
             const tx=await contract.PayforTherapy(doctors.metaid,{
-              value:ethers.utils.parseEther(feeinEth),
+
+              value:parseEther(feeinEth),
+
             });
 
             await tx.wait();
             alert("Payment succcessful");
+            setTimeout(()=>{
+             router.push('/client'),100;
+            })
           
           }
         catch(error){
           console.error("payment error",error)
-          alert("payment failed")
+          alert(`payment Payment failed: ${error?.message || "Unknown error"}`)
         }
     }
   return (
@@ -67,7 +103,7 @@ export default function Payment() {
             onClick={handlePayment}
             className="mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
           >
-            Pay {doctors.fees} ETH
+            Pay {doctors.fees} INR
           </button>
         
         </>):(

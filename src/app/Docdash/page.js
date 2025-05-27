e97225263e178ca0720775806e2a3731c6a8281c
swastik1283 <swastik1283kot@gmail.com>
2025-05-28 02:04:40 +0530

    Docdash updated

--- src/app/Docdash/page.js
+++ src/app/Docdash/page.js
@@ -6,23 +6,30 @@ const Docdash = () => {
 const[doctors,setdoctors]=useState(null);
 
 useEffect(()=>{
+  const fetchDoctor = async () => {
+    try {
+      const res = await fetch("/api/auth/DoctorSession", {
+        method: "GET",
+        credentials: "include", // ✅ this sends cookies
+      });
 
-    const fetchdoctor=async()=>{
-        const res=await fetch("/api/auth/DoctorSession",{
-           credentials:"include"
-        })
-        console.log(res);
-        if(res.ok){
-            const data= await res.json();
-           setdoctors(data)
-        }
-       
-    
+      if (!res.ok) {
+        console.error("❌ Failed to fetch doctor");
+        return;
+      }
+
+      const { data } = await res.json();
+      setdoctors(data);
+    } catch (err) {
+      console.error("Error fetching doctor", err);
     }
-    fetchdoctor();
-},[])
+  };
+
+  fetchDoctor();
+}, []);
 
 if(!doctors){
+   
     return <p>Loading...</p>
 }
     return (
@@ -30,16 +37,18 @@ if(!doctors){
             <div className=" flex flex-row p-2 justify-between h-full ">
                 <div className="flex-col items-center text-center w-[80%]  border-r border-gray-200">
                     <h1 className="bg-[#e8d696] text-black text-3xl text-center font-bold p-5">Recent Appointment</h1>
+                 
                 </div>
      <div className=" flex-col items-end  w-[20%] ">
 
         <Image className="mx-23 mt-8" src="/assets/Doc.png" width={150} height={100} alt='docimg'/>
 
       <h1 className="text-2xl font-bold mb-4  mt-4  text-center text-black">Welcome Dr. {doctors.fname}</h1>
-      <p><strong>Email:</strong> {doctors.email}</p>
+    <div className=" font-bold mb-4  mt-4  text-center text-black">  <p><strong>Email:</strong> {doctors.email}</p>
       <p><strong>Specialization:</strong> {doctors.role}</p>
       <p><strong>Fees:</strong> {doctors.fees}</p>
       <p><strong>Metamask:</strong> {doctors.metaid}</p>
+    </div>
     </div>
     </div>
     </section>
